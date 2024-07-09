4ec5be0db7f08e23a4a35fa591f80881ef0f21a3
Ameen <157805698+ameenPms@users.noreply.github.com>
2024-07-09 09:37:24 +0400

    send message, get messages, search conversation

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,15 +1,30 @@
 import React from "react";
+import useConversation from "../../zustand/useConversation";
+import {useAuthContext} from "../../context/AuthContext"
+import { extractTime } from "../../utils/extractTime";
 
-const Message = () => {
+const Message = ( {message} ) => {
+  const { authUser } = useAuthContext();
+  const {selectedConversation} = useConversation()
+  const isFromMe = message.senderId === authUser._id
+  const formattedTime = extractTime(message.createdAt)
+  const chatClassName = isFromMe ? 'chat-end':'chat-start'
+  const profilePic = isFromMe ? authUser.profilePic : selectedConversation.profilePic
+  const bubbleBgColor = isFromMe ? 'bg-green-500' : ' '
   return (
-    <div className="chat chat-start">
+    <div className={`chat ${chatClassName}`}>
       <div className="chat-image avater">
         <div className="w-10 rounded-full">
-          <img src="" alt="img" />
+          <img
+            src={profilePic}
+            alt="img"
+          />
         </div>
       </div>
-      <div className="chat-bubble text-white bg-blue-500">Hi whats up</div>
-      <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">12:42</div>
+      <div className={`chat-bubble text-white bg-blue-500 ${bubbleBgColor}`}>{message.message}</div>
+      <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">
+        {formattedTime}
+      </div>
     </div>
   );
 };
--- frontend/src/pages/signup/GenderCheckBox.jsx
+++ frontend/src/pages/signup/GenderCheckBox.jsx
@@ -1,18 +1,32 @@
 import React from "react";
 
-const GenderCheckBox = () => {
+const GenderCheckBox = ({onCheckBoxChange, selectedGender}) => {
   return (
     <div className="flex ">
       <div className="form-control">
         <label htmlFor="" className={"label gap-2 cursor-pointer"}>
           <span className="label-text">Male</span>
-          <input type="checkbox" className="checkbox border-slate-900" />
+          <input
+            type="checkbox"
+            checked={selectedGender === "male"}
+            onChange={() => onCheckBoxChange("male")}
+            className={`checkbox border-slate-900 ${
+              selectedGender === "male" ? "selected" : ""
+            }`}
+          />
         </label>
       </div>
       <div className="form-control">
         <label htmlFor="" className={"label gap-2 cursor-pointer"}>
           <span className="label-text">Female</span>
-          <input type="checkbox" className="checkbox border-slate-900" />
+          <input
+            type="checkbox"
+            checked={selectedGender === "female"}
+            onChange={() => onCheckBoxChange("female")}
+            className={`checkbox border-slate-900 ${
+              selectedGender === "female" ? "selected" : ""
+            }`}
+          />
         </label>
       </div>
     </div>
