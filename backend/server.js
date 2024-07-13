c15f8e479ad72cd210fabef2ca813b9a1b5d7b00
Ameen <157805698+ameenPms@users.noreply.github.com>
2024-07-13 12:44:46 +0400

    added static field middleware

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
@@ -12,6 +13,8 @@ import { app, server } from './socket/socket.js'
 
 const PORT = process.env.PORT || 8080
 
+const __dirname = path.resolve()
+
 
 dotenv.config()
 
@@ -22,12 +25,17 @@ app.use('/api/auth', authRoutes)
 app.use('/api/messages', messageRoutes)
 app.use('/api/users', userRoutes)
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
 
-
-
-app.get('/', (req, res)=>{
-    res.send('Hello world')
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'))
 })
+
+
+
+// app.get('/', (req, res)=>{
+//     res.send('Hello world')
+// })
 
 
 server.listen(PORT, () => {
